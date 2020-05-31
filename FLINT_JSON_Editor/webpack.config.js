7b3c44c296baf7134c5ef9a551a2021be2b832b1
Abhishek Garain <abhi211199@gmail.com>
2020-06-01 01:22:43 +0530

    created new electron-webpack project

---
+++ FLINT_JSON_Editor/webpack.config.js
@@ -0,0 +1,8 @@
+const mainConfig = require("./webpack.main.config");
+const rendererConfig = require("./webpack.renderer.config");
+
+const config = [mainConfig, rendererConfig];
+
+module.exports = config;
+
+console.log(module.exports)
