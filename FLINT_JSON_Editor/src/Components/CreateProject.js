8f86ec4789bb7dceea89d8dd7c157c2ef91f6d81
Abhishek Garain <abhi211199@gmail.com>
2020-06-12 21:31:31 +0530

    added UI for CreateProject

--- FLINT_JSON_Editor/src/Components/CreateProject.js
+++ FLINT_JSON_Editor/src/Components/CreateProject.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles,createMuiTheme, ThemeProvider  } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
@@ -9,7 +9,16 @@ import Tooltip from '@material-ui/core/Tooltip';
 const folder = "./src/storage/templates/";
 const fs = require("fs");
 import jsonw from '../storage/packages_metadata.json';
+import Button from '@material-ui/core/Button';
+import { green } from '@material-ui/core/colors';
+import Input from '@material-ui/core/Input';
+const { dialog } = require('electron').remote;
 
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+  },
+});
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
@@ -34,13 +43,35 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(2),
     right: theme.spacing(3),
   },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  // root: {
+  //   '& > *': {
+  //     margin: theme.spacing(1),
+  //   },
+  // },
 }));
+
+function getPath(json_value){
+  console.log("aa")
+  dialog.showOpenDialog({
+    properties: ['openDirectory']
+  }).then(result => {
+    console.log(result.canceled)
+    console.log(result.filePaths)
+    ReactDOM.render(<Input style={{width:"500px"}} value={result.filePaths+"/"+json_value} disabled inputProps={{ 'aria-label': 'description' }} />,document.getElementById("path_input"));
+  }).catch(err => {
+    console.log(err)
+  })
+}
 
 export default function TitlebarGridList() {
   const classes = useStyles();
-  const [selectedValue, setSelectedValue] = React.useState(0);
+  const [selectedValue, setSelectedValue] = React.useState(false);
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
+    console.log(event.target.value);
   };
 {
   var temp=[];
@@ -63,11 +94,11 @@ export default function TitlebarGridList() {
       subtitle={<span>{jsonw[x]["description"]}</span>}
       actionIcon={
       <Radio
-        checked={selectedValue === i.toString()}
+        checked={selectedValue === x}
         onChange={handleChange}
-        value={i.toString()}
+        value={x}
         name="radio-button-demo"
-        inputProps={{ 'aria-label': i.toString() }}
+        inputProps={{ 'aria-label': x }}
         />
       }
     />
@@ -79,6 +110,17 @@ export default function TitlebarGridList() {
 
   return (
     <div className={classes.root}>
+      <div style={{display:"inline-flex"}}>
+        <form className={classes.root} noValidate autoComplete="off" id="path_input">
+          {/* <Input style={{width:"400px"}} value="" disabled inputProps={{ 'aria-label': 'description' }} /> */}
+        </form>
+        <ThemeProvider theme={theme}>
+          <Button variant="contained" color="primary" className={classes.margin} onClick={()=>{getPath(selectedValue);}}>
+            Choose Path
+          </Button>
+        </ThemeProvider>
+      </div>
+
       <GridList cellHeight={300} className={classes.gridList}  >
         {temp}
       </GridList>
