9af3f2fb7265a00073c6d96d757e9a611857a23b
Abhishek Garain <abhi211199@gmail.com>
2020-06-17 01:16:24 +0530

    added transfer list in CreateCFG

--- FLINT_JSON_Editor/src/renderer.js
+++ FLINT_JSON_Editor/src/renderer.js
@@ -29,7 +29,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Test from './Components/Tabs'
+import Test from './Components/CreateCFG'
 // import './css/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
