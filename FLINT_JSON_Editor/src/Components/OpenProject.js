c6cf22de86a2157fa212a8d113487a47779dc8e6
Abhishek Garain <abhi211199@gmail.com>
2020-06-23 22:16:18 +0530

    added OpenProject feature

--- FLINT_JSON_Editor/src/Components/OpenProject.js
+++ FLINT_JSON_Editor/src/Components/OpenProject.js
@@ -1,5 +1,96 @@
 import React from 'react';
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Chip from '@material-ui/core/Chip';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+import { green } from '@material-ui/core/colors';
+const {dialog} = require('electron').remote;
+const {basename} = require('path');
+import Tooltip from '@material-ui/core/Tooltip';
+const fs = require("fs");
 
-export default function OpenProject(){
-    return(<div>Feature Under Development</div>);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    listStyle: 'none',
+    padding: theme.spacing(0.5),
+    margin: 0,
+  },
+  chip: {
+    margin: theme.spacing(0.5),
+  },
+  margin: {
+    margin: theme.spacing(1),
+    marginLeft: "40%",
+  },
+}));
+
+const theme = createMuiTheme({
+    palette: {
+      primary: green,
+    },
+});
+
+export default function ChipsArray() {
+  const classes = useStyles();
+  const [chipData, setChipData] = React.useState([
+    // { key: 0, label: 'Angular' },
+    // { key: 1, label: 'jQuery' },
+    // { key: 2, label: 'Polymer' },
+    // { key: 3, label: 'React' },
+    // { key: 4, label: 'Vue.js' },
+  ]);
+  
+  console.log(chipData);
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) => chips.filter((chip) => chip !== chipToDelete));
+  };
+
+  function selectFiles() {
+    dialog.showOpenDialog({
+        properties: ['openDirectory']
+      }).then(result => {
+        console.log(result);
+        console.log(result.canceled)
+        console.log(result.filePaths)
+        fs.readdir(result.filePaths[0],(err,files)=>{
+            var temp=[];
+            for(var i in files)
+            {
+                // setChipData([...chipData.concat({key: i,label: result.filePaths[i]})])
+                temp.push(result.filePaths+"/"+files[i]);
+            }
+            setChipData([...new Set(chipData.concat(temp))]);
+            console.log(chipData);
+            setDisp(true);
+        });
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+
+  return (
+    <div>
+        <ThemeProvider theme={theme}>
+            <Button variant="contained" color="primary" className={classes.margin} onClick={()=>{selectFiles()}}>Select Files</Button>
+        </ThemeProvider>
+        <Paper component="ul" className={classes.root}>
+        { chipData.map((data) => {
+            return (
+            <li>
+              <Tooltip title={data}>
+                <Chip
+                label={basename(data)}
+                onDelete={handleDelete(data)}
+                className={classes.chip}
+                />
+              </Tooltip>
+            </li>
+            );
+        })}
+        </Paper>
+    </div>
+  );
 }
