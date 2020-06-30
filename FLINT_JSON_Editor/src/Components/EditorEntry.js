60139acf8c0261aee0e639a4109dc9e85f05ba68
Abhishek Garain <abhi211199@gmail.com>
2020-07-01 00:29:49 +0530

    added dialog in EditorEntry

--- FLINT_JSON_Editor/src/Components/EditorEntry.js
+++ FLINT_JSON_Editor/src/Components/EditorEntry.js
@@ -3,12 +3,15 @@ import update from 'react-addons-update';
 import Tabs from "react-draggable-tabs";
 import Editor from './ScratchJSONEditor';
 const fs = require("fs");
+import MyDialog from './Dialog';
+import { dialog } from 'electron';
 
 export default function EditorEntry(props) {
   // const classes = useStyles();
   props={
     files: ["standard_gcbm_variables.json","standard_gcbm_spinup.json","standard.json"]
   };
+  props.notFound=["aa"];
   const [tabs,setTabs] = React.useState([
     {
         id: 1,
@@ -28,6 +31,7 @@ export default function EditorEntry(props) {
     },
 ]);
 const [view,setView] = React.useState(false);
+const [dialogDisp, setdialogDisp] = React.useState(false);
 
 function moveTab(dragIndex, hoverIndex) {
   let newTabs = [...tabs];
@@ -35,10 +39,19 @@ function moveTab(dragIndex, hoverIndex) {
   setTabs(newTabs);
 }
 
-console.log(props.files);
+// console.log(props.files);
 fs.readdir('src/storage/templates/files',(err,files)=>{
   if(err) throw err;
-  console.log(files)
+  console.log(files);
+  // props.notFound=[];
+  var map=new Map();
+  for(var i=0;i<files.length;i++)
+    map.set(files[i],1);
+  for(var i=0;i<props.files.length;i++)
+    if(!(map.get(props.files[i])))
+      props.notFound.push(props.files[i]);
+  console.log(props.notFound.length==0);
+  setdialogDisp(true);
 });
 
 function selectTab(selectedIndex, selectedID) {
@@ -56,7 +69,6 @@ function selectTab(selectedIndex, selectedID) {
   {document.getElementById("v1").style.display="none";document.getElementById("v2").style.display="none";document.getElementById("v3").style.display="block";}
 
 }
-console.log(view);
 
 function closedTab(removedIndex, removedID) {
   let newTabs = [...tabs]
@@ -86,6 +98,11 @@ document.body.style.backgroundImage='none';
 
 return (
     <div>
+      { dialogDisp && <MyDialog message={props.notFound.length==0 ? "You have chosen "+props.files+" to open in the editor. Choose mode!" : "You have chosen "+props.files+" to open in the editor. Choose mode!(Templates for "+props.notFound+" were not found so they will be automatically opened in ScratchJSONEditor regardless of chosen option)" }
+                heading="JSON Editor"
+                positive="Template Editor"
+                negative="ScratchJSONEditor!"
+                reply={(ans)=>{console.log(ans)}} />}
       <Tabs moveTab={moveTab} selectTab={selectTab} closeTab={closedTab} tabs={tabs}>
         <button onClick={addTab}>+</button>
       </Tabs>
