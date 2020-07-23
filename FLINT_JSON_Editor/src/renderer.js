2ca6befb4a821de5b069557ed5197b0cb60f4ef4
Abhishek Garain <abhi211199@gmail.com>
2020-07-24 00:43:43 +0530

    added pools

--- FLINT_JSON_Editor/src/renderer.js
+++ FLINT_JSON_Editor/src/renderer.js
@@ -29,7 +29,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import Test from './Components/Forms/variables';
+import Test from './Components/Forms/pools';
 import {ToggleEditorEntryProvider,EditorEntryFilesProvider} from './Components/ContextManager';
 // import './css/index.css'
 
