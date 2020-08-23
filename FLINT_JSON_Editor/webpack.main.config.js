23bdc3c46588c236578fe60ca61e30845ec352ba
Abhishek Garain <abhi211199@gmail.com>
2020-08-23 17:19:29 +0530

    added icons and build methods for linux

--- FLINT_JSON_Editor/webpack.main.config.js
+++ FLINT_JSON_Editor/webpack.main.config.js
@@ -1,3 +1,7 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const assets = [ 'Images', 'storage', 'css' ]; // asset directories
+const path = require('path');
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
@@ -8,4 +12,12 @@ module.exports = {
   module: {
     rules: require('./webpack.rules'),
   },
+  // logo is copied in .webpack/main for mainWindow icon
+  plugins: [new CopyWebpackPlugin({
+    patterns: [{
+      from:path.resolve(__dirname, 'src/Images/logo.png'),
+      to: path.resolve(__dirname, '.webpack/main')
+    }],
+  })]
+
 };
